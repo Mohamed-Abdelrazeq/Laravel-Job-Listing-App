62bafb6f4aa62fc1f806c082170812388e4552dc
Mohamed <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2024-09-30 10:51:04 +0300

    fix tags display

--- resources/views/job-listings.blade.php
+++ resources/views/job-listings.blade.php
@@ -16,7 +16,7 @@
                     @foreach ($job->tags as $tag)
                         <span
                             class="inline-block bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
-                            {{ $tag }}
+                            {{ $tag->name }}
                         </span>
                     @endforeach
                 </div>
