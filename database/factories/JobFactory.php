67db64e5c625019ca1925230d8b9d3c701e483e4
Mohamed <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2024-09-30 05:05:32 +0300

    factory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -17,7 +17,8 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->jobTitle,
+            'salary' => fake()->numberBetween(40000, 200000)
         ];
     }
 }
