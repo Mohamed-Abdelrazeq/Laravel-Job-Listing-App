6d9672ae5905d1e4532956610a5fe8cca6ae6951
Mohamed <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2024-09-30 07:43:21 +0300

    create employer and connect to the job

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Job;
 
 class JobSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $employers = \App\Models\Employer::factory(10)->create();
+
+        $employers->each(function ($employer) {
+            $employer->jobListings()->saveMany(Job::factory(5)->make());
+        });
     }
 }
